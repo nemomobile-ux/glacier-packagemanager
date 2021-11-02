412e4962158d43593cfb26858ebcbb392d024ebe
Jozef Mlich <jmlich83@gmail.com>
2021-11-02 17:05:45 +0000

    lupdate

---
+++ translations/glacier-packagemanager.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>InstallPackagePage</name>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="33"/>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="47"/>
+        <source>Install package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="82"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="90"/>
+        <source>Installing package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="99"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="115"/>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="129"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="116"/>
+        <source>Package installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="130"/>
+        <source>Error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InstallPackagePage.qml" line="131"/>
+        <source>We have some error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="31"/>
+        <source>Package manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="66"/>
+        <source>Updating repo...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="74"/>
+        <source>Package manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RemovePackagePage</name>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="33"/>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="47"/>
+        <source>Remove package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="82"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="98"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="114"/>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="129"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="115"/>
+        <source>Package removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="130"/>
+        <source>Error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RemovePackagePage.qml" line="131"/>
+        <source>We have some error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesPage</name>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="36"/>
+        <source>Updates: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="37"/>
+        <source>Updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="47"/>
+        <source>System is updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="88"/>
+        <source>Update packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="102"/>
+        <source>Load updates list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="133"/>
+        <source>Updating...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="145"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/UpdatesPage.qml" line="146"/>
+        <source>Packages updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>Install &lt;package&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <location filename="../src/main.cpp" line="54"/>
+        <source>package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="53"/>
+        <source>Update packages.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="54"/>
+        <source>Remove &lt;package&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
