8ebc950af2bd661644dec15761a120b1736de6ee
Jozef Mlich <jmlich83@gmail.com>
2021-11-18 14:02:47 +0000

    lupdate-qt5 . -recursive -no-obsolete -ts ./translations/glacier-packagemanager.ts

--- translations/glacier-packagemanager.ts
+++ translations/glacier-packagemanager.ts
@@ -2,47 +2,68 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en">
 <context>
-    <name>InstallPackagePage</name>
+    <name>AuthorizationFailPage</name>
     <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="33"/>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="47"/>
-        <source>Install package</source>
+        <location filename="../qml/pages/AuthorizationFailPage.qml" line="33"/>
+        <source>Package manager</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="82"/>
-        <source>Install</source>
+        <location filename="../qml/pages/AuthorizationFailPage.qml" line="40"/>
+        <source>Authorization failed</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="90"/>
-        <source>Installing package</source>
-        <translation type="unfinished"></translation>
+</context>
+<context>
+    <name>HistoryItemDelegate</name>
+    <message numerus="yes">
+        <location filename="../qml/components/HistoryItemDelegate.qml" line="51"/>
+        <source>%n year(s) ago</source>
+        <comment>actions</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/components/HistoryItemDelegate.qml" line="53"/>
+        <source>%n months(s) ago</source>
+        <comment>actions</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/components/HistoryItemDelegate.qml" line="55"/>
+        <source>%n day(s) ago</source>
+        <comment>actions</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/components/HistoryItemDelegate.qml" line="57"/>
+        <source>%n hours(s) ago</source>
+        <comment>actions</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/components/HistoryItemDelegate.qml" line="59"/>
+        <source>%n minutes(s) ago</source>
+        <comment>actions</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
     </message>
     <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="99"/>
-        <source>Cancel</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="115"/>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="129"/>
-        <source>Ok</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="116"/>
-        <source>Package installed</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="130"/>
-        <source>Error!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InstallPackagePage.qml" line="131"/>
-        <source>We have some error</source>
+        <location filename="../qml/components/HistoryItemDelegate.qml" line="61"/>
+        <source>Just now</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -51,6 +72,16 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="34"/>
         <source>Package manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="72"/>
+        <source>Search packages:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="89"/>
+        <source>Last actions:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -98,48 +129,8 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/main.cpp" line="75"/>
+        <location filename="../src/main.cpp" line="77"/>
         <source>Package manager</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>RemovePackagePage</name>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="33"/>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="47"/>
-        <source>Remove package</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="82"/>
-        <source>Remove</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="98"/>
-        <source>Cancel</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="114"/>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="129"/>
-        <source>Ok</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="115"/>
-        <source>Package removed</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="130"/>
-        <source>Error!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RemovePackagePage.qml" line="131"/>
-        <source>We have some error</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -198,23 +189,23 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../src/main.cpp" line="53"/>
+        <location filename="../src/main.cpp" line="55"/>
         <source>Install &lt;package&gt;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="53"/>
         <location filename="../src/main.cpp" line="55"/>
+        <location filename="../src/main.cpp" line="57"/>
         <source>package</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="54"/>
+        <location filename="../src/main.cpp" line="56"/>
         <source>Update packages.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="55"/>
+        <location filename="../src/main.cpp" line="57"/>
         <source>Remove &lt;package&gt;</source>
         <translation type="unfinished"></translation>
     </message>
