693b616fa7def7b1b9f1d862eb51e2f1d6407bbd
Jozef Mlich <jmlich83@gmail.com>
2022-04-18 10:19:04 +0200

    lupdate

--- translations/glacier-packagemanager.ts
+++ translations/glacier-packagemanager.ts
@@ -4,12 +4,12 @@
 <context>
     <name>AuthorizationFailPage</name>
     <message>
-        <location filename="../qml/pages/AuthorizationFailPage.qml" line="33"/>
+        <location filename="../src/qml/pages/AuthorizationFailPage.qml" line="33"/>
         <source>Package manager</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/AuthorizationFailPage.qml" line="40"/>
+        <location filename="../src/qml/pages/AuthorizationFailPage.qml" line="40"/>
         <source>Authorization failed</source>
         <translation type="unfinished"></translation>
     </message>
@@ -17,7 +17,7 @@
 <context>
     <name>HistoryItemDelegate</name>
     <message numerus="yes">
-        <location filename="../qml/components/HistoryItemDelegate.qml" line="51"/>
+        <location filename="../src/qml/components/HistoryItemDelegate.qml" line="51"/>
         <source>%n year(s) ago</source>
         <comment>actions</comment>
         <translation type="unfinished">
@@ -26,7 +26,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../qml/components/HistoryItemDelegate.qml" line="53"/>
+        <location filename="../src/qml/components/HistoryItemDelegate.qml" line="53"/>
         <source>%n months(s) ago</source>
         <comment>actions</comment>
         <translation type="unfinished">
@@ -35,7 +35,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../qml/components/HistoryItemDelegate.qml" line="55"/>
+        <location filename="../src/qml/components/HistoryItemDelegate.qml" line="55"/>
         <source>%n day(s) ago</source>
         <comment>actions</comment>
         <translation type="unfinished">
@@ -44,7 +44,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../qml/components/HistoryItemDelegate.qml" line="57"/>
+        <location filename="../src/qml/components/HistoryItemDelegate.qml" line="57"/>
         <source>%n hours(s) ago</source>
         <comment>actions</comment>
         <translation type="unfinished">
@@ -53,7 +53,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../qml/components/HistoryItemDelegate.qml" line="59"/>
+        <location filename="../src/qml/components/HistoryItemDelegate.qml" line="59"/>
         <source>%n minutes(s) ago</source>
         <comment>actions</comment>
         <translation type="unfinished">
@@ -62,7 +62,7 @@
         </translation>
     </message>
     <message>
-        <location filename="../qml/components/HistoryItemDelegate.qml" line="61"/>
+        <location filename="../src/qml/components/HistoryItemDelegate.qml" line="61"/>
         <source>Just now</source>
         <translation type="unfinished"></translation>
     </message>
@@ -70,17 +70,17 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="34"/>
+        <location filename="../src/qml/pages/MainPage.qml" line="34"/>
         <source>Package manager</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="72"/>
+        <location filename="../src/qml/pages/MainPage.qml" line="72"/>
         <source>Search packages:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="89"/>
+        <location filename="../src/qml/pages/MainPage.qml" line="89"/>
         <source>Last actions:</source>
         <translation type="unfinished"></translation>
     </message>
@@ -88,17 +88,17 @@
 <context>
     <name>OrphansPage</name>
     <message>
-        <location filename="../qml/pages/OrphansPage.qml" line="36"/>
+        <location filename="../src/qml/pages/OrphansPage.qml" line="36"/>
         <source>Orphaned packages</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/OrphansPage.qml" line="63"/>
+        <location filename="../src/qml/pages/OrphansPage.qml" line="63"/>
         <source>Cleanup</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/OrphansPage.qml" line="80"/>
+        <location filename="../src/qml/pages/OrphansPage.qml" line="80"/>
         <source>Loading orphaned packages list</source>
         <translation type="unfinished"></translation>
     </message>
@@ -106,22 +106,22 @@
 <context>
     <name>PackageInfoPage</name>
     <message>
-        <location filename="../qml/pages/PackageInfoPage.qml" line="121"/>
+        <location filename="../src/qml/pages/PackageInfoPage.qml" line="121"/>
         <source>New version</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/PackageInfoPage.qml" line="121"/>
+        <location filename="../src/qml/pages/PackageInfoPage.qml" line="121"/>
         <source>Version</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/PackageInfoPage.qml" line="133"/>
+        <location filename="../src/qml/pages/PackageInfoPage.qml" line="133"/>
         <source>Installed version</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/PackageInfoPage.qml" line="147"/>
+        <location filename="../src/qml/pages/PackageInfoPage.qml" line="147"/>
         <source>Repo</source>
         <translation type="unfinished"></translation>
     </message>
@@ -137,23 +137,23 @@
 <context>
     <name>TransactionPage</name>
     <message>
-        <location filename="../qml/pages/TransactionPage.qml" line="32"/>
-        <location filename="../qml/pages/TransactionPage.qml" line="47"/>
+        <location filename="../src/qml/pages/TransactionPage.qml" line="32"/>
+        <location filename="../src/qml/pages/TransactionPage.qml" line="47"/>
         <source>Package manager working...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/TransactionPage.qml" line="81"/>
+        <location filename="../src/qml/pages/TransactionPage.qml" line="81"/>
         <source>Waiting...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/TransactionPage.qml" line="88"/>
+        <location filename="../src/qml/pages/TransactionPage.qml" line="88"/>
         <source>Downloading...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/TransactionPage.qml" line="95"/>
+        <location filename="../src/qml/pages/TransactionPage.qml" line="95"/>
         <source>Preparing...</source>
         <translation type="unfinished"></translation>
     </message>
@@ -161,27 +161,27 @@
 <context>
     <name>UpdatesPage</name>
     <message>
-        <location filename="../qml/pages/UpdatesPage.qml" line="37"/>
+        <location filename="../src/qml/pages/UpdatesPage.qml" line="37"/>
         <source>Updates: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/UpdatesPage.qml" line="38"/>
+        <location filename="../src/qml/pages/UpdatesPage.qml" line="38"/>
         <source>Updates</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/UpdatesPage.qml" line="68"/>
+        <location filename="../src/qml/pages/UpdatesPage.qml" line="68"/>
         <source>Update packages</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/UpdatesPage.qml" line="93"/>
+        <location filename="../src/qml/pages/UpdatesPage.qml" line="93"/>
         <source>System is up to date</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/UpdatesPage.qml" line="78"/>
+        <location filename="../src/qml/pages/UpdatesPage.qml" line="78"/>
         <source>Loading updates list</source>
         <translation type="unfinished"></translation>
     </message>
